4b78ebbd0bf9f4fbf843d07e40c201825fd3a040
SatyaveniSukapatla <sukapatlasatyaveni@gmail.com>
2025-10-30 22:05:14 +0530

    Add to cart button interactive

--- Js-scripts/amazon.js
+++ Js-scripts/amazon.js
@@ -27,7 +27,7 @@
 let productsHTML = '';
 
 products.forEach((product) => {
-    productsHTML += `
+  productsHTML += `
        <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -40,7 +40,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
@@ -72,10 +72,40 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" 
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
 });
-console.log(productsHTML);
+// console.log(productsHTML);
+
 document.querySelector('.js-products-data').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    })
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+
+    console.log(cart);
+  });
+});
+
+
+
